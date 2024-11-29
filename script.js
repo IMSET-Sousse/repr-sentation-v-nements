23bf552cedd418002dfff684c3836f98a58bf78d
amoulazrm <88037785+amoulazrm@users.noreply.github.com>
2024-11-29 02:11:13 +0100

    categories and details

--- script.js
+++ script.js
@@ -20,3 +20,28 @@ carousel.addEventListener('mousemove', function () {
     const bootstrapCarousel = new bootstrap.Carousel(carousel);
     bootstrapCarousel.next();
 });
+var eventModal = document.getElementById('eventModal');
+        eventModal.addEventListener('show.bs.modal', function (event) {
+            var button = event.relatedTarget; // Button that triggered the modal
+            var eventName = button.getAttribute('data-event-name');
+            var eventImage = button.getAttribute('data-image');
+            var eventDescription = button.getAttribute('data-description');
+            var eventPlace = button.getAttribute('data-place');
+            var eventTime = button.getAttribute('data-time');
+            var eventDate = button.getAttribute('data-date');
+
+            // Update the modal's content
+            var modalTitle = eventModal.querySelector('.modal-title');
+            var modalImage = eventModal.querySelector('#eventModalImage');
+            var modalDescription = eventModal.querySelector('#eventModalDescription');
+            var modalPlace = eventModal.querySelector('#eventModalPlace');
+            var modalTime = eventModal.querySelector('#eventModalTime');
+            var modalDate = eventModal.querySelector('#eventModalDate');
+
+            modalTitle.textContent = eventName;
+            modalImage.src = eventImage;
+            modalDescription.textContent = eventDescription;
+            modalPlace.textContent = eventPlace;
+            modalTime.textContent = eventTime;
+            modalDate.textContent = eventDate;
+        });
